630117669245f5cdf577eacaa20e2600707638ab
Victor-Lasky <victormoura99075719@gmail.com>
2025-02-14 01:02:06 +0000

    certo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -4,14 +4,16 @@ int main() {
 
 //variáveis
     char estado1[20], estado2[20], cidade1[20], resultado[20], cidade2[20];
-    int codigo1, codigo2, populacao1, populacao2, turisticos1, turisticos2, atributo1, atributo2,modo,comparacao;
+    int codigo1, codigo2, populacao1, populacao2, turisticos1, turisticos2, atributo,modo,comparacao,menu;
     float area1, area2, pib1, pib2;
 
 //Código para inserir manualmente os dados das cartas.
-   printf("Bem vindo ao Super Trunfo Cidades.");
-   printf("____Menu Inicial___");
+   printf("Bem vindo ao Super Trunfo Cidades.\n");
+   printf("____Menu Inicial___\n");
    printf("1- Jogar\n2- Regras\n3- Sair\n");
-   
+   scanf("%d",&menu);
+   switch(menu){
+       case 1:
    
     printf("Carregando...\n\nJogador 1:\n\n");
     printf("Digite o código da carta:\n");
@@ -78,20 +80,17 @@ switch(modo){
        
        case 1:
     //código para comparacao de atributos separadamente        
-    printf("Escolha 1 atributo para ser calculado em cada carta:\n");
+    printf("Escolha 1 atributo para ser comparado entre as cartas:\n");
     printf("1- Comparar qual carta tem a maior População\n");
     printf("2- Comparar qual carta tem a maior Área\n");
     printf("3- Comparar qual carta tem o maior PIB\n");
     printf("4- Comparar qual carta tem o maior número de Pontos Turísticos.\n");
     printf("5- Comparar qual carta tem a menor Densidade Populacional\n");
     
-    printf("Escolha o primeiro atributo:\n");
-    scanf("%d", &atributo1);
+    printf("Escolha o atributo a ser comparado:\n");
+    scanf("%d", &atributo);
 
-    printf("Escolha o segundo atributo:\n");
-    scanf("%d", &atributo2);
- 
- switch (atributo1){
+ switch (atributo){
     case 1:
 
         if (populacao1>populacao2){
@@ -135,69 +134,22 @@ switch(modo){
             printf("A carta contendo a cidade com menor densidade populacional é:%s\n",cidade2);
          }
          break;
-        default:
-        printf("Opcão inválida. Tente Novamente.\n");
- }
         
-switch (atributo2){
-    case 1:
-
-        if (populacao1>populacao2){
-              printf("A carta contendo a cidade com maior populacão é %s:\n",cidade1);
-        }else{
-
-        printf("A carta contendo a cidade com maior populacão é %s:\n",cidade2);
-    }
  
-       break;
-     case 2:
-
-        if (area1>area2){
-             printf("A carta contendo a cidade com maior área é %s\n",cidade1);
-        }else{
-            printf("A carta contendo a cidade com maior área é %s\n",cidade2);
-        }
-     
-       break;
-     case 3:
-        if (pib1<pib2){
-            printf("A carta contendo a cidade com maior o maior PIB é %s\n",cidade2);
-        
-        }else{
-            printf("A carta contendo a cidade com maior o maior PIB é %s\n",cidade1);
-        }
-        
-      
-       break;
-
-     case 4:
-        if (turisticos1>turisticos2){
-            printf("A carta contendo a cidade com maior número de Pontos turisticos é %s\n",cidade1);
-        }else{
-            printf("A carta contendo a cidade com maior número de Pontos turisticos é %s\n",cidade2);
-        }
-      break;
-
-      case 5:
-
-      if(densidade1<densidade2){
-        printf("A carta contendo a cidade com menor densidade populacional é:%s\n",cidade1);
-     }else{
-        printf("A carta contendo a cidade com menor densidade populacional é:%s\n",cidade2);
-     }
-     break;
+        }   
+        break;
         default:
         printf("Opcão inválida. Tente Novamente.\n");
- }
+ 
 
              case 2:{
             
-            printf("Escolha dois atributos para serem comparados em:\n");
-            printf("obs: Vencerá a carta que tiver exito nos dois atributos.");
+            printf("Escolha dois atributos para serem comparados:\n");
+            printf("obs: Vencerá a carta que tiver exito nos dois atributos.\n");
             printf("1- Comparar a Área e Populacão.\n");
             printf("2- Comparar o PIB e Densidade Populacional\n");
             printf("3- Comparar os Pontos Turísticos e Área.\n");
-            scanf("%d",comparacao);
+            scanf("%d",&comparacao);
 
 
 switch(comparacao){
@@ -237,7 +189,19 @@ switch(comparacao){
             }
      
 
-        }
+         }
+    
+    break;
+   
+
+        } 
+    case 2:
+
+    printf("toledo");
+
+
+    } 
+
+
     return 0;
-}
 }
